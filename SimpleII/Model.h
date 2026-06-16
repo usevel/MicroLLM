2f439c2d5ec7c9928f52949b1878e9bfaebdb22b
eokaaa <kostya2079@gmail.com>
2026-06-17 01:32:23 +0500

    сделана более умная модель

---
+++ SimpleII/Model.h
@@ -0,0 +1,154 @@
+#pragma once
+
+#include <vector>
+#include <numeric>
+#include <cstdlib>
+
+namespace Models
+{
+	class SimpleModel
+	{
+	public:
+		std::vector<std::vector<float>> MatrixWeight;
+		std::vector<float> Biases;
+
+
+		SimpleModel(int InSize, int OutSize) : InputSize(InSize), OutputSize(OutSize)
+		{
+			MatrixWeight.resize(OutputSize, std::vector<float>(InputSize, 0.f));
+			Biases.resize(OutputSize, 0.f);
+		}
+		~SimpleModel() = default;
+
+		std::vector<float> Forward(const std::vector<float>& InputUser)
+		{
+			std::vector<float> Result(OutputSize, 0.f);
+
+			for (int i = 0; i < OutputSize; ++i)
+			{
+				Result[i] = Biases[i];
+				for (int j = 0; j < InputSize; ++j)
+					Result[i] += MatrixWeight[i][j] * InputUser[j];
+			}
+
+			return Result;
+		}
+
+		void MachineLearning(const std::vector<float>& InputUser, const std::vector<float>& Target)
+		{
+			std::vector<float> Predict = Forward(InputUser);
+
+			for (int i = 0; i < OutputSize; ++i)
+			{
+				float Error = Predict[i] - Target[i];
+				Biases[i] -= (LearnRate * Error);
+
+				for (int j = 0; j < InputSize; ++j)
+					MatrixWeight[i][j] -= (LearnRate * Error * InputUser[j]);
+			}
+		}
+
+	private:
+		int InputSize = 0;
+		int OutputSize = 0;
+		float LearnRate = 0.1f;
+	};
+
+
+	class DeepModel
+	{
+	public:
+		std::vector<std::vector<float>> MatrixWeight1;
+		std::vector<float> Biases1;
+
+		std::vector<std::vector<float>> MatrixWeight2;
+		std::vector<float> Biases2;
+
+		std::vector<float> HiddenRaw;
+		std::vector<float> Hidden;
+		std::vector<float> Predict;
+
+		DeepModel(int InSize, int HidSize, int OutSize)
+			: InputSize{ InSize }, HiddenSize{ HidSize }, OutputSize{ OutSize }
+		{
+			Biases1.resize(HiddenSize, 0.f);
+			Biases2.resize(OutputSize, 0.f);
+
+			MatrixWeight1.resize(HiddenSize, std::vector<float>(InputSize));
+			MatrixWeight2.resize(OutputSize, std::vector<float>(HiddenSize));
+
+			for (int i = 0; i < HiddenSize; ++i)
+				for (int j = 0; j < InputSize; ++j)
+					MatrixWeight1[i][j] = (float(rand() % 100) / 1000.f) - 0.05f;
+
+			for (int i = 0; i < OutputSize; ++i)
+				for (int j = 0; j < HiddenSize; ++j)
+					MatrixWeight2[i][j] = (float(rand() % 100) / 1000.f) - 0.05f;
+
+			HiddenRaw.resize(HiddenSize, 0.f);
+			Hidden.resize(HiddenSize, 0.f);
+			Predict.resize(OutputSize, 0.f);
+		}
+		~DeepModel() = default;
+
+		std::vector<float> Forward(const std::vector<float>& InputUser)
+		{
+			for (int i = 0; i < HiddenSize; ++i)
+			{
+				HiddenRaw[i] = Biases1[i];
+				for (int j = 0; j < InputSize; ++j)
+					HiddenRaw[i] += MatrixWeight1[i][j] * InputUser[j];
+
+				Hidden[i] = (HiddenRaw[i] < 0 ? 0.f : HiddenRaw[i]);
+			}
+
+			for (int i = 0; i < OutputSize; ++i)
+			{
+				Predict[i] = Biases2[i];
+				for (int j = 0; j < HiddenSize; ++j)
+					Predict[i] += MatrixWeight2[i][j] * Hidden[j];
+			}
+
+			return Predict;
+		}
+
+		void MachineLearning(const std::vector<float>& InputUser, const std::vector<float>& Target)
+		{
+			Forward(InputUser);
+
+			std::vector<float> ErrorOutput(OutputSize, 0.f);
+			for (int i = 0; i < OutputSize; ++i)
+			{
+				ErrorOutput[i] = Predict[i] - Target[i];
+				Biases2[i] -= (LearnRate * ErrorOutput[i]);
+
+				for (int j = 0; j < HiddenSize; ++j)
+					MatrixWeight2[i][j] -= (LearnRate * ErrorOutput[i] * Hidden[j]);
+			}
+
+			std::vector<float> ErrorHidden(HiddenSize, 0.f);
+			for (int i = 0; i < HiddenSize; ++i)
+			{
+				for (int j = 0; j < OutputSize; ++j)
+					ErrorHidden[i] += ErrorOutput[j] * MatrixWeight2[j][i];
+
+				if (HiddenRaw[i] <= 0)
+					ErrorHidden[i] = 0.f;
+			}
+
+			for (int i = 0; i < HiddenSize; ++i)
+			{
+				Biases1[i] -= (LearnRate * ErrorHidden[i]);
+
+				for (int j = 0; j < InputSize; ++j)
+					MatrixWeight1[i][j] -= (LearnRate * ErrorHidden[i] * InputUser[j]);
+			}
+		}
+
+	private:
+		int InputSize = 0;
+		int HiddenSize = 0;
+		int OutputSize = 0;
+		float LearnRate = 0.05f;
+	};
+}
--- SimpleII/main.cpp
+++ SimpleII/main.cpp
@@ -1,136 +1,20 @@
 #include <iostream>
-#include <vector>
-#include <unordered_map>
 #include <string>
-#include <sstream>
+#include <vector>
 
-class Tokenizator
-{
-public:
-
-	std::unordered_map<std::string, int> WordToIndex;
-
-	Tokenizator() = default;
-	~Tokenizator() = default;
-
-	std::vector<std::string> CleanString(std::string& String)
-	{
-		std::string CleanText = String;
-		std::vector<std::string> ArrayString;
-
-		for (auto& Ch : CleanText)
-		{
-			if (Ch == ',' || Ch == '.' ||
-				Ch == '!' || Ch == '?' || 
-				Ch == '-' || Ch == '=' || 
-				Ch == '+')
-				Ch = ' ';
-		}
-			
-		std::stringstream SS(CleanText);
-		std::string Word;
-		while (SS >> Word)
-			ArrayString.push_back(Word);
-		
-		return ArrayString;
-	}
-
-	void BringToMap(std::vector<std::string>& SetWords)
-	{
-		int index = 0;
-
-		for (auto& SetWord : SetWords)
-		{
-			std::vector<std::string> Words = CleanString(SetWord);
-
-			for (auto& Word : Words)
-			{
-				if (WordToIndex.find(Word) == WordToIndex.end())
-				{
-					WordToIndex[Word] = index;
-					++index;
-				}
-			}
-		}
-	}
-
-	std::vector<float> FloatVector(std::string InputText)
-	{
-		std::vector<float> Result(WordToIndex.size(), 0.f);
-		std::vector<std::string> Words = CleanString(InputText);
-
-		for (auto& Word : Words)
-		{
-			auto it = WordToIndex.find(Word);
-			if (it != WordToIndex.end())
-				Result[it->second] = 1.f;
-		}
-
-		return Result;
-	}
-};
-
-class SimpleII
-{
-public:
-	std::vector<std::vector<float>> MatrixWeight;
-	std::vector<float> Biases;
-
-
-	SimpleII(int InSize, int OutSize) : InputSize(InSize), OutputSize(OutSize)
-	{
-		MatrixWeight.resize(OutputSize, std::vector<float>(InputSize, 0.f));
-		Biases.resize(OutputSize, 0.f);
-	}
-	~SimpleII() = default;
-
-	std::vector<float> Forward(const std::vector<float>& InputUser)
-	{
-		std::vector<float> Result(OutputSize, 0.f);
-
-		for (int i = 0; i < OutputSize; ++i)
-		{
-			Result[i] = Biases[i];
-			for (int j = 0; j < InputSize; ++j)
-				Result[i] += MatrixWeight[i][j] * InputUser[j];
-		}
-
-		return Result;
-	}
-
-	void MachineLearning(const std::vector<float>& InputUser, const std::vector<float>& Target)
-	{
-		std::vector<float> Predict = Forward(InputUser);
-
-		for (int i = 0; i < OutputSize; ++i)
-		{
-			float Error = Predict[i] - Target[i];
-			Biases[i] -= (LearnRate * Error);
-
-			for (int j = 0; j < InputSize; ++j)
-			{
-				MatrixWeight[i][j] -= (LearnRate * Error * InputUser[j]);
-			}
-		}
-	}
-
-private:
-	int InputSize = 0;
-	int OutputSize = 0;
-	float LearnRate = 0.1f;
-};
-
-
+#include "Model.h"
+#include "Tokenizator.h"
 
 int main()
 {
 	std::vector<std::string> TestPhrases = {
-		"привет",
-		"как дела",
-		"пока"
+		"фильм плохой",
+		"фильм хороший",
+		"не плохой",
+		"не хороший"
 	};
 
-	Tokenizator Token;
+	Tokens::Tokenizator Token;
 	Token.BringToMap(TestPhrases);
 
 	std::cout << "Словарь:\n";
@@ -139,13 +23,14 @@ int main()
 
 	std::cout << "\n-----------------------------------\n\n";
 
-	SimpleII II(Token.WordToIndex.size(), 3);
+	Models::DeepModel II(Token.WordToIndex.size(), 8, 2);
 
-	for (int i = 0; i < 1000; ++i)
+	for (int i = 0; i < 5000; ++i)
 	{
-		II.MachineLearning(Token.FloatVector("привет"), { 1.f, 0.f, 0.f });
-		II.MachineLearning(Token.FloatVector("как дела"), { 0.f, 1.f, 0.f });
-		II.MachineLearning(Token.FloatVector("пока"), { 0.f, 0.f, 1.f });
+		II.MachineLearning(Token.FloatVector("фильм плохой"),	{ 0.f, 1.f });
+		II.MachineLearning(Token.FloatVector("фильм хороший"),	{ 1.f, 0.f });
+		II.MachineLearning(Token.FloatVector("не плохой"),		{ 1.f, 0.f });
+		II.MachineLearning(Token.FloatVector("не хороший"),		{ 0.f, 1.f });
 	}
 
 	while (true)
@@ -162,21 +47,9 @@ int main()
 
 		std::vector<float> Predict = II.Forward(DetectWords);
 
-		int BestIndex = 0;
-		float MaxValue = Predict[0];
-		for (int i = 0; i < Predict.size(); ++i)
-		{
-			if (Predict[i] > MaxValue)
-			{
-				MaxValue = Predict[i];
-				BestIndex = i;
-			}
-		}
-
 		std::cout << "AI вывод: ";
-		if (BestIndex == 0)			std::cout << "И тебе привет!\n\n";
-		else if (BestIndex == 1)	std::cout << "Всё как обычно!\n\n";
-		else if (BestIndex == 2)	std::cout << "До скорого!\n\n";
+		if (Predict[0] > Predict[1])		std::cout << "Хороший\n\n";
+		else if (Predict[0] < Predict[1])	std::cout << "Плохой\n\n";
 	}
 
 }
